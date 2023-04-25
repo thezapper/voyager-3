d29ee551b55ed9ee2639dedd4bdb3a1defd6699f
Gary Gray <aintgottimetobleed@gmail.com>
2023-04-25 10:57:06 +0100

    Added colour to planets, fixed some config issues.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(
   {
     plugins: [react()],
-    server: {port:80},
+    server: {
+      port:80,
+      host:"127.0.0.1",
+      strictPort:true
+    },
     root: "client/src",
     publicDir: "client/public",
     build: {
